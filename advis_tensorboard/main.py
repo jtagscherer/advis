03db1e50d111995ff7f7b9a1d08fdcca463e24b7
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-03-10 19:21:13 +0100

    Added framework code that builds and launches a plugin

---
+++ advis_tensorboard/main.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+from tensorboard import default
+from tensorboard import program
+import tensorflow as tf
+
+from advis_plugin import advis_plugin
+
+if __name__ == '__main__':
+  plugins = default.get_plugins() + [advis_plugin.AdvisPlugin]
+  assets = os.path.join(tf.resource_loader.get_data_files_path(), 'assets.zip')
+  program.main(plugins, lambda: open(assets, 'rb'))
