ff987bdcee20a5120465e742e9a7d135cd1c15c7
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-03-22 20:39:53 +0100

    Improved the transformation of input to image visualization tensors in performance and prettiness

--- advis_plugin/layer_summary.py
+++ advis_plugin/layer_summary.py
@@ -37,35 +37,12 @@ def op(name, data, display_name=None, description=None, collections=None):
 		plugin_data=tf.SummaryMetadata.PluginData(plugin_name=PLUGIN_NAME)
 	)
 	
-	images = _generate_image_from_tensor(data)
-	
-	with tf.name_scope('convert_to_uint8'):
-		images = tf.stack(
-			[tf.cast(255 * tf.clip_by_value(image_, 0.0, 1.0), tf.uint8)
-			for image_ in images]
-		)
-	
-	with tf.name_scope(name):
-		encoded_images = tf.map_fn(
-			tf.image.encode_png,
-			images,
-			dtype=tf.string,
-			name='encode_each_image'
-		)
-
-		image_shape = tf.shape(images)
-		dimensions = tf.stack(
-			[tf.as_string(image_shape[1], name='width'),
-			tf.as_string(image_shape[2], name='height')],
-			name='dimensions'
-		)
-
-		tensor = tf.concat([dimensions, encoded_images], axis=0)
+	images = _generate_image_from_tensor(data, name_scope=name)
 
 	# Return a summary op that is properly configured.
 	return tf.summary.tensor_summary(
 		name=name,
-		tensor=tensor,
+		tensor=images,
 		summary_metadata=summary_metadata,
 		collections=collections
 	)
@@ -98,29 +75,47 @@ def pb(tag, data, display_name=None, description=None):
 										tensor=tensor)
 	return summary
 	
-def _generate_image_from_tensor(tensor):
-	"""Transform an input tensor as obtained from the graph into a list of 
-	tensors that each describe an appropriate image visualization of a unit's
-	activation in the layer described by the whole tensor.
+def _generate_image_from_tensor(tensor, name_scope=None):
+	"""Transform an input tensor as obtained from the graph into a tensor that 
+	describes all appropriate image visualizations of each unit's activation in 
+	the layer.
 
 	Arguments:
 		tensor: An input tensor of shape [1, width, height, index]`.
+		name_scope: Optional name scope for all tensor operations
 	"""
 	
-	# Unstack the data to get rid of the first dimension
-	tensor = tf.unstack(tensor)[0]
+	with tf.name_scope(name_scope):
+		# Unstack the data to get rid of the first dimension
+		tensor = tf.unstack(tensor)[0]
+		
+		# Fill in RGB channels for each image tensor
+		tensor = tf.map_fn(lambda x: tf.stack([x, x, x]), tensor)
+		
+		# Transpose axes so that image indices are first and RGB channels last
+		tensor = tf.transpose(tensor, [3, 0, 2, 1])
+		
+		# Stack tensors and clip and multiply values of the RGB channels
+		tensors = tf.stack(
+			tf.map_fn(
+				lambda x: tf.cast(255 * tf.clip_by_value(x, 0.0, 1.0), tf.uint8),
+				tensor,
+				dtype=tf.uint8
+			)
+		)
+		
+		# Encode all images
+		encoded_images = tf.map_fn(tf.image.encode_png, tensors, dtype=tf.string)
+		
+		# Calculate dimensions
+		image_shape = tf.shape(tensors)
+		dimensions = tf.stack(
+			[tf.as_string(image_shape[1], name='width'),
+			tf.as_string(image_shape[2], name='height')],
+			name='dimensions'
+		)
+		
+		# Concatenate dimensions and encoded images for the final image tensor
+		image_tensor = tf.concat([dimensions, encoded_images], axis=0)
 	
-	# Transpose axes so the image index is first, followed by its dimensions
-	tensor = tf.transpose(tensor, [2, 0, 1])
-	
-	# Unstack images by their indices
-	tensors = tf.unstack(tensor)
-	
-	# Fill in RGB channels for each image tensor
-	tensors = [tf.stack([image_tensor, image_tensor, image_tensor])
-						for image_tensor in tensors]
-	
-	# Transpose axes again so that RGB channels are last for each image
-	tensors = [tf.transpose(image_tensor, [1, 2, 0]) for image_tensor in tensors]
-	
-	return tensors
+	return image_tensor
