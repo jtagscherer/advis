d1ce8b2184decdf670bf863ccb04dff732925064
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-03-22 11:47:03 +0100

    Now displaying images from the plugin route in the layer image component

--- advis_plugin/layer_image/layer-image.ts
+++ advis_plugin/layer_image/layer-image.ts
@@ -5,6 +5,7 @@ Polymer({
   properties: {
     run: String,
     tag: String,
+		url: String,
 		
     requestManager: Object,
     _canceller: {
@@ -24,27 +25,15 @@ Polymer({
   reload() {
     this._fetchNewData(this.run, this.tag);
   },
+	
   _fetchNewData(run, tag) {
     if (this._attached) {
-			this._canceller.cancelAll();
-			
-			// TODO: Retrieve image data from our plugin
-      const url = tf_backend.addParams(tf_backend.getRouter()
-				.pluginRoute('images', '/images'), {
+			// Update the URL of the current image shown
+      this.url = tf_backend.addParams(tf_backend.getRouter()
+				.pluginRoute('advis', '/layerImage'), {
         tag: this.tag,
         run: this.run
       });
-			
-	    const updateData = this._canceller.cancellable(result => {
-				console.log(result)
-				
-	      if (!result.cancelled) {
-					// TODO: Load the image into our container
-					const backendData = result.value;
-	      }
-	    });
-			
-	    this.requestManager.request(url).then(updateData);
     }
   }
 });
