66a117b4d5798bd116a7dc1b157a8e9882d3b544
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-03-24 12:04:14 +0100

    Now simply displaying one static layer image visualization instead of a list

--- advis_plugin/dashboard/dashboard.ts
+++ advis_plugin/dashboard/dashboard.ts
@@ -3,13 +3,9 @@
 Polymer({
   is: 'advis-dashboard',
   properties: {
-    _selectedRuns: Array,
-    _runToTag: Object,  // map<run: string, tags: string[]>
+    _currentRun: String,
+    _currentTag: String,
     _dataNotFound: Boolean,
-    _categories: {
-      type: Array,
-      computed: '_makeCategories(_runToTag, _selectedRuns)'
-    },
     _requestManager: {
       type: Object,
       value: () => new tf_backend.RequestManager()
@@ -21,32 +17,25 @@ Polymer({
   },
   reload() {
     this._fetchTags().then(() => {
-      this._reloadCards();
+      this.$$('layer-image').reload();
     });
   },
 	
   _fetchTags() {
     const url = tf_backend.getRouter().pluginRoute('advis', '/tags');
+		
     return this._requestManager.request(url).then(runToTag => {
-      if (_.isEqual(runToTag, this._runToTag)) {
-        // No need to update anything if there are no changes.
-        return;
-      }
-      const tags = tf_backend.getTags(runToTag);
-      this.set('_dataNotFound', tags.length === 0);
-      this.set('_runToTag', runToTag);
+			// Check for data availability before assigning variables
+			if (Object.keys(runToTag).length > 0) {
+				// Statically use both the first run and the first tag for now
+				this._currentRun = Object.keys(runToTag)[0];
+				this._currentTag = runToTag[this._currentRun][0];
+				
+				this._dataNotFound = false;
+			} else {
+				this._dataNotFound = true;
+			}
     });
-  },
-	
-  _reloadCards() {
-    this.querySelectorAll('layer-image').forEach(g => {
-      g.reload();
-    });
-  },
-	
-  _makeCategories(runToTag, selectedRuns) {
-    return tf_categorization_utils.categorizeRunTagCombinations(
-			runToTag, selectedRuns);
   }
 });
 
