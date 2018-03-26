cce102ee46dbd19a35011f6426daa58f2bdea884
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-03-26 16:16:29 +0200

    Added a utility function to check requests for completeness

--- advis_plugin/argutil.py
+++ advis_plugin/argutil.py
@@ -1,8 +1,6 @@
-"""
-Utility methods for dealing with command line parameters.
-"""
-
 import os
+
+from tensorboard.backend import http_util
 
 def check_directory_validity(parser, arg):
   if not os.path.exists(arg):
@@ -15,3 +13,31 @@ def check_directory_validity(parser, arg):
     parser.error('The directory {} does not exist!'.format(arg))
   else:
     return arg
+
+def check_missing_arguments(request, arguments):
+	"""Check whether a request contains all necessary arguments.
+	
+	Arguments:
+		request: The request to be checked.
+		arguments: A list of all arguments that should be contained in the 
+			request.
+	Returns:
+		None if no arguments are missing, or an appropriate response if some are in 
+		fact missing.
+	"""
+	missing_arguments = []
+	
+	for argument in arguments:
+		if argument not in request.args:
+			missing_arguments.append(argument)
+	
+	if len(missing_arguments) > 0:
+		return http_util.Respond(
+			request,
+			'Your request is missing the following necessary arguments: {}.'
+				.format(' ,'.join(missing_arguments)),
+			'text/plain',
+			code=400
+		)
+	else:
+		return None
