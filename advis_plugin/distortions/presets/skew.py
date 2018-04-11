42d686fc345fcea80735b181d552392c21c4a592
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-04-11 20:59:36 +0200

    Added a skew distortion preset

---
+++ advis_plugin/distortions/presets/skew.py
@@ -0,0 +1,25 @@
+from skimage.transform import warp, AffineTransform
+
+def get_display_name():
+	return 'Skew'
+
+def get_parameters():
+	return [
+		{
+			'name': 'skew_range',
+			'display_name': 'Rotation Range',
+			'type': 'range',
+			'constraints': {
+				'min': 0,
+				'max': 1
+			},
+			'default': {
+				'lower': 0,
+				'upper': 0.5
+			}
+		}
+	]
+
+def distort(image, configuration):
+	transformation = AffineTransform(shear=configuration['skew_range'])
+	return warp(image, inverse_map=transformation, mode='symmetric')
