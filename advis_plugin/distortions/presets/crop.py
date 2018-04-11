40ddd1c7cc02aa853fe720d645c864990f2507ce
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-04-11 20:38:58 +0200

    Added a crop distortion preset

---
+++ advis_plugin/distortions/presets/crop.py
@@ -0,0 +1,55 @@
+def get_display_name():
+	return 'Crop'
+
+def get_parameters():
+	return [
+		{
+			'name': 'zoom_factor',
+			'display_name': 'Zoom Factor',
+			'type': 'constant',
+			'constraints': {
+				'min': 1,
+				'max': 10
+			},
+			'default': 2
+		},
+		{
+			'name': 'vertical_crop_range',
+			'display_name': 'Vertical Crop Range',
+			'type': 'range',
+			'constraints': {
+				'min': 0,
+				'max': 1
+			},
+			'default': {
+				'lower': 0,
+				'upper': 1
+			}
+		},
+		{
+			'name': 'horizontal_crop_range',
+			'display_name': 'Horizontal Crop Range',
+			'type': 'range',
+			'constraints': {
+				'min': 0,
+				'max': 1
+			},
+			'default': {
+				'lower': 0,
+				'upper': 1
+			}
+		}
+	]
+
+def distort(image, configuration):
+	original_height, original_width, _ = image.shape
+	
+	cropped_width = int(original_width / configuration['zoom_factor'])
+	cropped_height = int(original_height / configuration['zoom_factor'])
+	
+	left = int(configuration['vertical_crop_range'] \
+	 	* (original_width - cropped_width))
+	top = int(configuration['horizontal_crop_range'] \
+	 	* (original_height - cropped_height))
+	
+	return image[top : top + cropped_height, left : left + cropped_width]
