c0c508b2e78ddfd5e457141e5d913176ce0959a6
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-05-07 12:15:36 +0200

    Added a dataset module for NIPS 2017 image data

---
+++ advis_plugin/datasets/presets/nips_2017/nips_2017.py
@@ -0,0 +1,22 @@
+from os.path import dirname, join
+import json
+
+def get_display_name():
+	return 'NIPS 2017 Adversarial Learning Challenge'
+
+def get_all_images():
+	# Retrieve image descriptions from their file
+	images_file = join(dirname(__file__), 'data/images.json')
+	
+	with open(images_file, 'r') as images_data:
+		images = json.load(images_data)
+	
+	# Replace image IDs with their full file name
+	image_base_path = join(dirname(__file__), 'data/images')
+	
+	return [
+		{
+			'path': join(image_base_path, '{}.png'.format(image['id'])),
+			'categoryId': image['categoryId'],
+			'categoryName': image['categoryName']
+		} for image in images]
