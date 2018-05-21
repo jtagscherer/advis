b948bee77596fdb0bf79da37ed794b6aa06bea9b
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-05-21 18:27:26 +0200

    Now clipping normal image visualizations to any percentage

--- advis_plugin/dashboard/layer_visualization/comparisons/swipe/swipe.ts
+++ advis_plugin/dashboard/layer_visualization/comparisons/swipe/swipe.ts
@@ -13,5 +13,28 @@ Polymer({
 			this.$$('#container').offsetWidth,
 			this.$$('#container').offsetHeight
 		)
+	},
+	
+	urlsChanged: function(urlType) {
+		this._adjustClipRectangle(0.5);
+	},
+	
+	sizeChanged: function() {
+		this._adjustClipRectangle(0.5);
+	},
+	
+	_adjustClipRectangle: function(percentage) {
+		let container = this.$$('#container').getBoundingClientRect();
+		let images = this.$$('#distorted-images').getBoundingClientRect();
+		
+		let leftEdge = images.left - container.left;
+		let rightEdge = images.right - container.left;
+		
+		this.customStyle['--clip-left'] = leftEdge + 'px';
+		this.customStyle['--clip-right'] = (leftEdge + (percentage * images.width))
+			+ 'px';
+		this.customStyle['--clip-bottom'] = images.height + 'px';
+		
+		this.updateStyles();
 	}
 } as any);
