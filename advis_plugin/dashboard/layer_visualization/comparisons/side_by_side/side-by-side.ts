e59214e455fc0885f0f35fe7205a4bd5dd9071d9
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-05-21 13:53:19 +0200

    Moved more common code into the comparison behavior

--- advis_plugin/dashboard/layer_visualization/comparisons/side_by_side/side-by-side.ts
+++ advis_plugin/dashboard/layer_visualization/comparisons/side_by_side/side-by-side.ts
@@ -3,83 +3,15 @@
 Polymer({
   is: 'side-by-side',
 	
-	properties: {
-		tileSize: Number,
-		_imagePadding: {
-			type: Number,
-			value: 2
-		}
-	},
-	
 	behaviors: [
 		VisualizationComparisonBehavior,
 		(Polymer as any).NeonAnimatableBehavior
 	],
 	
-	urlsChanged: function(urlType) {
-		this._updateTileSize();
-	},
-	
-	sizeChanged: function() {
-		this._updateTileSize();
-	},
-	
-	_updateTileSize: function() {
-		// In the following, tiles of normal and distorted images will be handled 
-		// in the same way since their amounts and container sizes will always be 
-		// equal.
-		if (this.normalUrls == null || this.normalUrls.length == 0
-			|| this.$$('#container') == null) {
-			return;
-		}
-		
-		// Get the minimum container size that we have to fill with tiles
-		let containerSize = Math.min(
+	getImageContainerSize: function() {
+		return Math.min(
 			this.$$('#container').offsetWidth / 2,
 			this.$$('#container').offsetHeight
 		)
-		
-		// Calculate the amount of images per row such that we get as close as 
-		// possible to a square arrangement
-		let imagesPerRow = Math.ceil(Math.sqrt(this.normalUrls.length));
-		
-		
-		// Set the size of all individual image tiles
-		this.set('tileSize', (containerSize / imagesPerRow) + this._imagePadding);
-		
-		// Finally, update the width of the image tile container to be able to 
-		// center it horizontally
-		this.customStyle['--image-container-width'] = 
-			((imagesPerRow + 1) * (this.tileSize + this._imagePadding)) + 'px';
-		this.updateStyles();
-	},
-	
-	tileTapped: function(e) {
-		let inputType = e.target.dataset.inputType;
-		let unitIndex = e.target.dataset.unitIndex;
-		
-		var unitTitle = `Tensor ${Number(unitIndex) + 1}`;
-		var unitImageUrl;
-		
-		if (inputType == 'distorted') {
-			unitTitle += ' (Distorted)';
-			unitImageUrl = this.distortedUrls[unitIndex];
-		} else {
-			unitImageUrl = this.normalUrls[unitIndex];
-		}
-		
-		// Show the enlarged image tile in a dialog
-		this.$$('unit-details-dialog').open({
-			model: {
-				title: this.model.displayName,
-				caption: `Version ${this.model.version}`
-			},
-			unit: {
-				title: unitTitle,
-				caption: this.layer
-			},
-			url: unitImageUrl,
-			animationTarget: e.target.getBoundingClientRect()
-		});
 	}
 } as any);
--- advis_plugin/dashboard/layer_visualization/comparisons/visualization-comparison-behavior.ts
+++ advis_plugin/dashboard/layer_visualization/comparisons/visualization-comparison-behavior.ts
@@ -11,6 +11,11 @@ const VisualizationComparisonBehavior = {
 		},
 		normalUrls: Array,
 		distortedUrls: Array,
+		tileSize: Number,
+		_imagePadding: {
+			type: Number,
+			value: 2
+		},
 		_requestManager: {
 			type: Object,
 			value: () => new tf_backend.RequestManager()
@@ -32,7 +37,7 @@ const VisualizationComparisonBehavior = {
 	},
 	
 	listeners: {
-		'iron-resize': 'sizeChanged'
+		'iron-resize': '_sizeChanged'
 	},
 	
 	behaviors: [
@@ -54,12 +59,76 @@ const VisualizationComparisonBehavior = {
 		}
   },
 	
+	_sizeChanged: function() {
+		this._updateTileSize();
+		this.sizeChanged();
+	},
+	
 	urlsChanged: function(urlType) {
 		// Can be implemented by components using this behavior
 	},
 	
 	sizeChanged: function() {
 		// Can be implemented by components using this behavior
+	},
+	
+	openUnitDialog: function(e) {
+		let inputType = e.target.dataset.inputType;
+		let unitIndex = e.target.dataset.unitIndex;
+		
+		var unitTitle = `Tensor ${Number(unitIndex) + 1}`;
+		var unitImageUrl;
+		
+		if (inputType == 'distorted') {
+			unitTitle += ' (Distorted)';
+			unitImageUrl = this.distortedUrls[unitIndex];
+		} else {
+			unitImageUrl = this.normalUrls[unitIndex];
+		}
+		
+		// Show the enlarged image tile in a dialog
+		this.$$('unit-details-dialog').open({
+			model: {
+				title: this.model.displayName,
+				caption: `Version ${this.model.version}`
+			},
+			unit: {
+				title: unitTitle,
+				caption: this.layer
+			},
+			url: unitImageUrl,
+			animationTarget: e.target.getBoundingClientRect()
+		});
+	},
+	
+	getImageContainerSize: function() {
+		// Has to be implemented by components using this behavior
+	},
+	
+	_updateTileSize: function() {
+		// In the following, tiles of normal and distorted images will be handled 
+		// in the same way since their amounts and container sizes will always be 
+		// equal.
+		if (this.normalUrls == null || this.normalUrls.length == 0
+			|| this.$$('#container') == null) {
+			return;
+		}
+		
+		// Get the container size that we have to fill with tiles
+		let containerSize = this.getImageContainerSize();
+		
+		// Calculate the amount of images per row such that we get as close as 
+		// possible to a square arrangement
+		let imagesPerRow = Math.ceil(Math.sqrt(this.normalUrls.length));
+		
+		// Set the size of all individual image tiles
+		this.set('tileSize', (containerSize / imagesPerRow) + this._imagePadding);
+		
+		// Finally, update the width of the image tile container to be able to 
+		// center it horizontally
+		this.customStyle['--image-container-width'] = 
+			((imagesPerRow + 1) * (this.tileSize + this._imagePadding)) + 'px';
+		this.updateStyles();
 	},
 	
 	_fetchNewData: function(model, layer) {
@@ -102,6 +171,8 @@ const VisualizationComparisonBehavior = {
 			}
 			
 			this.normalUrls = tileUrls;
+			
+			this._updateTileSize();
 			this.urlsChanged('normal');
 		});
 	},
@@ -134,6 +205,8 @@ const VisualizationComparisonBehavior = {
 			}
 			
 			this.distortedUrls = tileUrls;
+			
+			this._updateTileSize();
 			this.urlsChanged('distorted');
 		});
 	}
