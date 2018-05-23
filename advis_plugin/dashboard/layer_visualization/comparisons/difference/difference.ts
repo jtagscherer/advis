03e9262e012d36339b14b2e215c2989236a2e661
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-05-23 19:22:57 +0200

    Now displaying visualization differences

--- advis_plugin/dashboard/layer_visualization/comparisons/difference/difference.ts
+++ advis_plugin/dashboard/layer_visualization/comparisons/difference/difference.ts
@@ -1,7 +1,16 @@
 'use strict';
 
+declare var resemble: any;
+
 Polymer({
   is: 'difference-comparison',
+	
+	properties: {
+		differenceImages: {
+			type: Array,
+			value: []
+		}
+	},
 	
 	behaviors: [
 		VisualizationComparisonBehavior,
@@ -24,6 +33,40 @@ Polymer({
 	},
 	
 	_calculateImageDifferences: function() {
-		// TODO: Calculate the image differences between each unit
+		if (this.normalUrls == null || this.normalUrls.length == 0
+			|| this.distortedUrls == null || this.distortedUrls.length == 0
+			|| this.normalUrls.length != this.distortedUrls.length) {
+			return;
+		}
+		
+    var computedImages = new Array(this.normalUrls.length);
+		let self = this;
+    
+    // This function will be called after every completed tile calculation
+    let checkCompletion = function(computedImages) {
+      // Only continue if all tile calculations have been completed
+      for (var result of computedImages) {
+        if (result == null) {
+          return;
+        }
+      }
+      
+      // Now that all calculations have been completed, update the image URL 
+      // array that is being displayed in the UI
+      self.set('differenceImages', []);
+      
+      for (var image of computedImages) {
+        self.push('differenceImages', image);
+      }
+    };
+		
+    // Calculate the image differences between each unit
+		for (var index in this.normalUrls) {
+			resemble(this.normalUrls[index]).compareTo(this.distortedUrls[index], 
+				index).onComplete(function(data) {
+				computedImages[Number(data.requestIndex)] = data.getImageDataUrl();
+				checkCompletion(computedImages);
+			});
+		}
 	}
 } as any);
