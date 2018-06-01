d586263decc676af031906d8b211e0728b9183be
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-06-01 15:05:18 +0200

    Added an empty graph settings dialog

---
+++ advis_plugin/dashboard/dialogs/graph_settings_dialog/graph-settings-dialog.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+Polymer({
+  is: 'graph-settings-dialog',
+	behaviors: [
+		DialogBehavior
+	],
+	
+  properties: {
+		// TODO
+		
+		eventId: {
+			type: String,
+			value: 'graph-settings-dialog'
+		}
+  },
+	
+	setContent: function(content) {
+		// TODO
+	}
+});
