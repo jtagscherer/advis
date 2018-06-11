cf4d98c5d0d7c97afd5206b9145a05ea42fe54dd
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-06-11 12:11:07 +0200

    Now using our own image difference calculation

--- advis_plugin/dashboard/layer_visualization/comparisons/difference/difference.ts
+++ advis_plugin/dashboard/layer_visualization/comparisons/difference/difference.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-declare var resemble: any;
+declare var chroma: any;
 
 Polymer({
   is: 'difference-comparison',
@@ -11,7 +11,11 @@ Polymer({
 		differenceMode: {
 			type: String,
 			value: 'difference-highlight',
-			observer: '_calculateImageDifferences'
+			observer: '_updateImages'
+		},
+		_colorScale: {
+			type: Object,
+			observer: '_updateImages'
 		}
 	},
 	
@@ -27,7 +31,7 @@ Polymer({
 		};
 	},
 	
-	getDialogImageSource: function(data, callback) {
+	/*getDialogImageSource: function(data, callback) {
 		// Retrieve the two tiles that will be compared
 		let unitIndex = data.selectedTile.index;
 		let originalUnit = this.getSingleTileImageUrl('original', unitIndex);
@@ -43,7 +47,7 @@ Polymer({
 		resembleControl.onComplete(function(data) {
 			callback(data.getImageDataUrl());
 		});
-	},
+	},*/
 	
 	getDialogTitle: function(data) {
 		let title = `Tensor ${Number(data.selectedTile.index) + 1}`;
@@ -67,11 +71,11 @@ Polymer({
 	},
 	
 	sizeChanged: function() {
-		this._calculateImageDifferences();
+		this._updateImages();
 	},
 	
 	stateChanged: function(state) {
-		this._calculateImageDifferences();
+		this._updateImages();
 	},
 	
 	_updateState: function() {
@@ -87,60 +91,111 @@ Polymer({
 		}
 	},
 	
-	_differenceImageCallback: function() {
-		this.set('_differenceImageLoaded', true);
-		this._updateState();
-	},
-	
-	_calculateImageDifferences: function() {
+	_updateImages: function() {
 		if (this.state == 'empty' || this._originalImageUrl == null
 			|| this._distortedImageUrl == null) {
 			return;
 		}
 		
-		this.set('_differenceImageUrl', null);
-		this.set('_differenceImageLoaded', false);
-		
 		let self = this;
 		
-		// Set up the Resemble comparator
-		var resembleControl = resemble(this._originalImageUrl)
-			.compareTo(this._distortedImageUrl)
-			.ignoreColors();
-		resembleControl = this._configureResembleControl(resembleControl);
-		
-		// Perform the comparison asynchronously and update the displayed image as 
-		// soon as it is loaded
-		resembleControl.onComplete(function(data) {
-			self.set('_differenceImageUrl', data.getImageDataUrl());
-		});
+		this._calculateImageDifference(
+			this._originalImageUrl,
+			this._distortedImageUrl,
+			function(result) {
+				self.set('_differenceImageUrl', result);
+			}
+		);
 	},
 	
-	_configureResembleControl: function(resembleControl) {
-		// Choose a color used to highlight differences
-		var outputSettings = {
-			errorColor: {
-				red: 244,
-				green: 112,
-				blue: 0
+	_differenceImageCallback: function() {
+		this.set('_differenceImageLoaded', true);
+		this._updateState();
+	},
+	
+	_calculateImageDifference: function(first, second, callback) {
+		let colorScale = chroma.scale('Spectral').domain([1, 0]);
+		
+		let canvas = document.createElement('canvas');
+		let context = canvas.getContext('2d');
+		
+		var firstImage = new Image();
+		firstImage.setAttribute('crossOrigin', 'anonymous');
+		var firstImageLoaded = false;
+		
+		var secondImage = new Image();
+		secondImage.setAttribute('crossOrigin', 'anonymous');
+		var secondImageLoaded = false;
+		
+		let calculateDifference = function() {
+			if (!firstImageLoaded || !secondImageLoaded) {
+				return;
 			}
+			
+			let width = firstImage.naturalWidth;
+			let height = firstImage.naturalHeight;
+			
+			canvas.width = width;
+			canvas.height = height;
+      
+      // Draw images onto canvases
+      let firstCanvas = document.createElement('canvas');
+      firstCanvas.width = width;
+      firstCanvas.height = height;
+  		let firstContext = firstCanvas.getContext('2d');
+      firstContext.drawImage(firstImage, 0, 0);
+      
+      let secondCanvas = document.createElement('canvas');
+      secondCanvas.width = width;
+      secondCanvas.height = height;
+  		let secondContext = secondCanvas.getContext('2d');
+      secondContext.drawImage(secondImage, 0, 0);
+      
+      // Fetch data of input images
+      let firstImageData = firstContext.getImageData(0, 0, width, height);
+      let secondImageData = secondContext.getImageData(0, 0, width, height);
+      
+      var differenceImageData = context.getImageData(0, 0, width, height);
+			
+			// Iterate through pixels, stored as RGBA data
+			for (var x = 0; x < width; x++) {
+				for (var y = 0; y < height; y++) {
+					// Get the data of both image's pixels
+					// Since we assume to be greyscale, we can simply use the red channel
+					let dataIndex = (x + (y * width)) * 4;
+					let firstValue = firstImageData.data[dataIndex] / 255;
+					let secondValue = secondImageData.data[dataIndex] / 255;
+					
+					// Calculate the difference and visualize it using a color scale
+					let valueDelta = Math.abs(firstValue - secondValue);
+					let currentColor = colorScale(valueDelta).get('rgba');
+					
+					// Assign values to the pixel's color channels
+					differenceImageData.data[dataIndex] = currentColor[0];
+          differenceImageData.data[dataIndex + 1] = currentColor[1];
+          differenceImageData.data[dataIndex + 2] = currentColor[2];
+          differenceImageData.data[dataIndex + 3] = currentColor[3] * 255;
+				}
+			}
+      
+      // Draw the image data onto the canvas
+      context.putImageData(differenceImageData, 0, 0);
+      
+      // Finally, return the result's data URL
+      callback(canvas.toDataURL('image/png'));
 		};
 		
-		// Choose a difference mode based on the user's mode selection
-		switch (this.differenceMode) {
-			case 'difference-highlight':
-				outputSettings['errorType'] = 'flat';
-				break;
-			case 'difference-intensity-highlight':
-				outputSettings['errorType'] = 'flatDifferenceIntensity';
-				break;
-			case 'only-difference':
-				outputSettings['errorType'] = 'diffOnly';
-				break;
-		}
+		firstImage.onload = function() {
+			firstImageLoaded = true;
+			calculateDifference();
+		};
 		
-		resembleControl.outputSettings(outputSettings);
+		secondImage.onload = function() {
+			secondImageLoaded = true;
+			calculateDifference();
+		};
 		
-		return resembleControl;
+		firstImage.src = first;
+		secondImage.src = second;
 	}
 } as any);
