a0b5f94fc9a07749981cccea8150af1b016e1b72
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-06-18 21:03:05 +0200

    Added the ResNetV2 model

---
+++ advis_plugin/models/presets/resnet_v2.py
@@ -0,0 +1,26 @@
+from os.path import join
+
+def get_display_name():
+	return 'ResNet V2'
+
+def get_version():
+	return 1.0
+
+def get_checkpoint_directory():
+	return {
+		'type': 'preset',
+		'directory': join('resnet_v2', 'resnet_v2_checkpoint')
+	}
+
+def get_dataset():
+	return 'nips_2017'
+
+def get_input_image_size():
+	return 224
+
+def get_output_node():
+	return 'Softmax'
+
+def annotate_node(node):
+	return 'resnet_v2' in node.name \
+	 	and node.op in ['Conv2D', 'Relu', 'MaxPool', 'AvgPool', 'ConcatV2']
