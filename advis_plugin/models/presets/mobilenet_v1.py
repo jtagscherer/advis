d9d13db1a17031e208f587055a49b3a6b686fb35
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-06-18 18:58:47 +0200

    Added the MobileNetV1 model

---
+++ advis_plugin/models/presets/mobilenet_v1.py
@@ -0,0 +1,25 @@
+from os.path import join
+
+def get_display_name():
+	return 'MobileNet V1'
+
+def get_version():
+	return 1.0
+
+def get_checkpoint_directory():
+	return {
+		'type': 'preset',
+		'directory': join('mobilenet_v1', 'mobilenet_v1_checkpoint')
+	}
+
+def get_dataset():
+	return 'nips_2017'
+
+def get_input_image_size():
+	return 224
+
+def get_output_node():
+	return 'Softmax'
+
+def annotate_node(node):
+	return node.op in ['Conv2D', 'Relu', 'MaxPool', 'AvgPool', 'ConcatV2']
