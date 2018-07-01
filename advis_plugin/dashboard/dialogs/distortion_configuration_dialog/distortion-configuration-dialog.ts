2035098036d97f13051e1cda0ed6cebef91aa8b1
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-07-01 19:25:07 +0200

    Restructured parameter configuration and added constant sliders

--- advis_plugin/dashboard/dialogs/distortion_configuration_dialog/distortion-configuration-dialog.ts
+++ advis_plugin/dashboard/dialogs/distortion_configuration_dialog/distortion-configuration-dialog.ts
@@ -8,19 +8,33 @@ Polymer({
 	
   properties: {
 		distortions: Array,
+		modifiedDistortions: Array,
 		requestManager: Object,
 		eventId: {
 			type: String,
 			value: 'distortion-configuration-dialog'
 		},
-		_selectedDistortion: {
+		_selectedDistortionIndex: {
 			type: Number,
 			value: 0
-		} 
+		}
   },
 	
-	_getSelectedDistortion: function(distortions, _selectedDistortion) {
-		return distortions[_selectedDistortion];
+	listeners: {
+		'parametersChangedEvent': '_parametersChanged'
+	},
+	
+	_parametersChanged: function(e) {
+		if (this.modifiedDistortions == null) {
+			return;
+		}
+		
+		this.modifiedDistortions[e.detail.distortionIndex].parameters =
+			e.detail.parameters;
+	},
+	
+	_getSelectedDistortion: function(distortions, _selectedDistortionIndex) {
+		return distortions[_selectedDistortionIndex];
 	},
 	
 	_isLastDistortionListItem: function(index) {
@@ -30,6 +44,11 @@ Polymer({
 	setContent: function(content) {
 		this.distortions = content.distortions;
 		this.requestManager = content.requestManager;
+		
+		// Copy the original distortions to allow lossless modification
+		this.set('modifiedDistortions', JSON.parse(JSON.stringify(
+			this.distortions)));
+		this.set('_selectedDistortionIndex', 0);
 	},
 	
 	getContentOnDismiss: function() {
---
+++ advis_plugin/dashboard/dialogs/distortion_configuration_dialog/parameter_configuration/constant_parameter/constant-parameter.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+Polymer({
+  is: 'constant-parameter',
+	properties: {
+		parameter: Object
+	}
+});
