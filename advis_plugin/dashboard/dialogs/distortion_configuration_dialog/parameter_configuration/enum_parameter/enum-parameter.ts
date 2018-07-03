2ad10988b5fdd40a7ecac59b1c1b025ec0e470c5
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-07-03 18:45:49 +0200

    Fixed a bug where the enum parameter UI would not update correctly

--- advis_plugin/dashboard/dialogs/distortion_configuration_dialog/parameter_configuration/enum_parameter/enum-parameter.ts
+++ advis_plugin/dashboard/dialogs/distortion_configuration_dialog/parameter_configuration/enum_parameter/enum-parameter.ts
@@ -4,5 +4,21 @@ Polymer({
   is: 'enum-parameter',
 	properties: {
 		parameter: Object
+	},
+	
+	observers: [
+	  '_valueChanged(parameter.value.name)'
+	],
+
+	_valueChanged: function(value) {
+	  var displayName = null;
+	  
+	  for (const option of this.parameter.options) {
+	    if (option.name == value) {
+	      displayName = option.displayName;
+	    }
+	  }
+	  
+	  this.set('parameter.value.displayName', displayName);
 	}
 });
