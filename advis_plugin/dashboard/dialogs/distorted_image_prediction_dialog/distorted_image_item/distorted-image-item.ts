b69e734d377fa438b5f3be5d3fbd5fa2e005ec8c
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-07-10 17:24:19 +0200

    Now displaying prediction certainties as a bar chart below distorted images

--- advis_plugin/dashboard/dialogs/distorted_image_prediction_dialog/distorted_image_item/distorted-image-item.ts
+++ advis_plugin/dashboard/dialogs/distorted_image_prediction_dialog/distorted_image_item/distorted-image-item.ts
@@ -4,6 +4,23 @@ Polymer({
   is: 'distorted-image-item',
 	properties: {
 		imageUrl: String,
-		certainty: Number
+		certainty: {
+			type: Number,
+			observer: '_certaintyChanged'
+		}
+	},
+	
+	_certaintyChanged: function(certainty) {
+		this.customStyle['--certainty-value'] = `${certainty * 100}%`;
+		this.updateStyles();
+	},
+	
+	_getFormattedValue: function(value) {
+		let roundedValue = (Math.round(value * 10000) / 10000.0).toFixed(4);
+		if (Number(roundedValue) * 100 < 0.01) {
+			return '<0.01%';
+		} else {
+			return `${(Number(roundedValue) * 100).toFixed(2)}%`;
+		}
 	}
 });
