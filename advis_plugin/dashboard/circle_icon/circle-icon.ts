7ed92bff07dbe44932a8e95ee135f3daf96c5161
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-07-11 11:58:24 +0200

    The circle icon now has the ability to display text

--- advis_plugin/dashboard/circle_icon/circle-icon.ts
+++ advis_plugin/dashboard/circle_icon/circle-icon.ts
@@ -3,6 +3,15 @@
 Polymer({
   is: 'circle-icon',
 	properties: {
-		icon: String
+		icon: String,
+		text: String
+	},
+	
+	_displayIcon: function(icon) {
+		return icon != null;
+	},
+	
+	_displayText: function(text) {
+		return text != null;
 	}
 });
