5381427ced914c6e0a5291a26b74f2fb3ba4d37c
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-07-11 17:37:16 +0200

    The font size within a circle icon can now be configured

--- advis_plugin/dashboard/circle_icon/circle-icon.ts
+++ advis_plugin/dashboard/circle_icon/circle-icon.ts
@@ -4,7 +4,12 @@ Polymer({
   is: 'circle-icon',
 	properties: {
 		icon: String,
-		text: String
+		text: String,
+		fontSize: {
+			type: Number,
+			value: 16,
+			observer: '_updateStyles'
+		}
 	},
 	
 	_displayIcon: function(icon) {
@@ -13,5 +18,10 @@ Polymer({
 	
 	_displayText: function(text) {
 		return text != null;
+	},
+	
+	_updateStyles: function() {
+		this.customStyle['--font-size'] = `${this.fontSize}px`;
+		this.updateStyles();
 	}
 });
