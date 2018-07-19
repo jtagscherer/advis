b044018e9adc024ef62a976869588c39cf05be12
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-07-19 22:09:29 +0200

    Replaced simple image positioning with better matrix transformations

--- advis_plugin/dashboard/dialogs/input_image_selection_dialog/confusion_matrix/zoomable_canvas/zoomable-canvas.ts
+++ advis_plugin/dashboard/dialogs/input_image_selection_dialog/confusion_matrix/zoomable_canvas/zoomable-canvas.ts
@@ -34,41 +34,43 @@ Polymer({
 		
 		canvas.addEventListener('mousewheel', function(e) {
 			e.preventDefault();
-			let zoomFactor = Math.pow(1.1, e.wheelDelta * 0.01) - 1;
 			
-			let cursorX = e.offsetX / (self.size * self._zoom);
-			let cursorY = e.offsetY / (self.size * self._zoom);
-			
-			self.zoom(zoomFactor);
-			self.translate(
-				self.size * zoomFactor * -0.5,
-				self.size * zoomFactor * -0.5
+			let position = self._context.transformedPoint(
+				e.offsetX, e.offsetY
 			);
+			
+			let factor = Math.pow(1.1, e.wheelDelta * 0.01);
+			
+			self._context.translate(position.x, position.y);
+			self._context.scale(factor, factor);
+			self._context.translate(-position.x, -position.y);
+			
+			self.redraw();
 		});
 		
 		var lastPosition = null;
 		
 		canvas.addEventListener('mousedown', function(e) {
 			e.preventDefault();
-			lastPosition = {
-				x: e.offsetX,
-				y: e.offsetY
-			};
+			lastPosition = self._context.transformedPoint(
+				e.offsetX, e.offsetY
+			);
 		});
 		
 		canvas.addEventListener('mousemove', function(e) {
 			e.preventDefault();
 			
 			if (lastPosition) {
-				self.translate(
-					e.offsetX - lastPosition.x,
-					e.offsetY - lastPosition.y
+				let position = self._context.transformedPoint(
+					e.offsetX, e.offsetY
 				);
 				
-				lastPosition = {
-					x: e.offsetX,
-					y: e.offsetY
-				};
+				self._context.translate(
+					position.x - lastPosition.x,
+					position.y - lastPosition.y
+				);
+				
+				self.redraw();
 			}
 		});
 		
@@ -96,6 +98,7 @@ Polymer({
 		
 		this._context = canvas.getContext('2d');
 		this._context.imageSmoothingEnabled = false;
+		this._enhanceContext(this._context);
 		
 		this._image = new Image();
 		
@@ -118,32 +121,84 @@ Polymer({
 		this._context.restore();
 		
 		// Draw the source image
-		// this._context.drawImage(this._image, 0, 0);
-		let imageSize = this._zoom * this.size;
-		
-		this._context.drawImage(
-			this._image, 0, 0, this._image.width, this._image.height,
-			this._x, this._y, imageSize, imageSize
-		);
+		this._context.drawImage(this._image, 0, 0);
 	},
 	
-	zoom: function(delta) {
-		this._zoom += delta;
-		this.redraw();
+	_enhanceContext: function(context) {
+		var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+		var matrix = svg.createSVGMatrix();
+		let self = this;
 		
-		/*if (this._context != null) {
-			let factor = Math.pow(1.1, delta);
-			
-			this._zoom += (factor - 1);
-			// this._context.scale(factor, factor);
-			this.redraw();
-			console.log(this._zoom);
-		}*/
-	},
-	
-	translate: function(x, y) {
-		this._x += x;
-		this._y += y;
-		this.redraw();
+		context.getTransformationMatrix = function() {
+			return matrix;
+		};
+
+		var savedTransformations = [];
+		
+		var save = context.save;
+		context.save = function() {
+			savedTransformations.push(matrix.translate(0, 0));
+			return save.call(context);
+		};
+
+		var restore = context.restore;
+		context.restore = function() {
+			matrix = savedTransformations.pop();
+			return restore.call(context);
+		};
+
+		var scale = context.scale;
+		context.scale = function(sx, sy) {
+			matrix = matrix.scaleNonUniform(sx, sy);
+			self._zoom += (sx - 1);
+			console.log(self._zoom);
+			return scale.call(context, sx, sy);
+		};
+
+		var rotate = context.rotate;
+		context.rotate = function(radians) {
+			matrix = matrix.rotate((radians * 180) / Math.PI);
+			return rotate.call(context, radians);
+		};
+
+		var translate = context.translate;
+		context.translate = function(dx, dy) {
+			matrix = matrix.translate(dx, dy);
+			self._x += dx;
+			self._y += dy;
+			// console.log(context.transformedPoint(0, 0));
+			return translate.call(context, dx, dy);
+		};
+
+		var transform = context.transform;
+		context.transform = function(a, b, c, d, e, f) {
+			var temp = svg.createSVGMatrix();
+			temp.a = a;
+			temp.b = b;
+			temp.c = c;
+			temp.d = d;
+			temp.e = e;
+			temp.f = f;
+			matrix = matrix.multiply(temp);
+			return transform.call(context, a, b, c, d, e, f);
+		};
+
+		var setTransform = context.setTransform;
+		context.setTransform = function(a, b, c, d, e, f) {
+			matrix.a = a;
+			matrix.b = b;
+			matrix.c = c;
+			matrix.d = d;
+			matrix.e = e;
+			matrix.f = f;
+			return setTransform.call(context, a, b, c, d, e, f);
+		};
+		
+		context.transformedPoint = function(x, y) {
+			var point = svg.createSVGPoint();
+			point.x = x;
+			point.y = y;
+			return point.matrixTransform(matrix.inverse());
+		}
 	}
 });
