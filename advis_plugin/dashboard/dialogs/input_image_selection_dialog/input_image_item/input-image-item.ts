a4770ea013c33f3989192ff1c2a8018471a04e7d
Jan Tagscherer <jan.tagscherer@gmail.com>
2018-07-24 18:08:36 +0200

    Now showing predictions and certainty charts in input images

--- advis_plugin/dashboard/dialogs/input_image_selection_dialog/input_image_item/input-image-item.ts
+++ advis_plugin/dashboard/dialogs/input_image_selection_dialog/input_image_item/input-image-item.ts
@@ -3,11 +3,36 @@
 Polymer({
   is: 'input-image-item',
 	properties: {
-		image: Object,
+		image: {
+			type: Object,
+			observer: '_imageChanged'
+		},
 		selected: {
 			type: Boolean,
 			observer: '_selectionChanged'
+		},
+		_certaintyDifferenceNegative: {
+			type: Boolean,
+			value: false
 		}
+	},
+	
+	_imageChanged: function(image) {
+		if (image == null) {
+			return;
+		}
+		
+		// Update the size of the certainty bars
+		this.customStyle['--original-certainty-value']
+			= `${image.certainty.original * 100}%`;
+		this.customStyle['--distorted-certainty-value']
+			= `${image.certainty.distorted * 100}%`;
+		this.customStyle['--certainty-difference-value']
+			= `${(Math.abs(image.certainty.difference) / 2) * 100}%`;
+		
+		this.set('_certaintyDifferenceNegative', image.certainty.difference < 0);
+		
+		this.updateStyles();
 	},
 	
 	_selectionChanged: function() {
@@ -15,6 +40,29 @@ Polymer({
 			this.$$('paper-card').elevation = 3;
 		} else {
 			this.$$('paper-card').elevation = 1;
+		}
+	},
+	
+	_getCertaintyDifferenceBarClass: function(negative) {
+		let baseClass = 'certainty-bar';
+		
+		if (negative) {
+			return `${baseClass} negative`;
+		} else {
+			return `${baseClass} positive`;
+		}
+	},
+	
+	_getFormattedValue: function(value) {
+		let roundedValue = (Math.round(value * 10000) / 10000.0).toFixed(4);
+		let percentage = Number(roundedValue) * 100;
+		
+		if (percentage < 0.01 && percentage > 0) {
+			return '<0.01%';
+		} else if (percentage > -0.01 && percentage < 0) {
+			return '>-0.01%';
+		} else {
+			return `${percentage.toFixed(2)}%`;
 		}
 	},
 	
@@ -34,7 +82,7 @@ Polymer({
 				computedClass += ' selected-text';
 			}
 		} else if (type == 'caption') {
-			computedClass += 'single-line monospaced caption';
+			computedClass += 'single-line caption';
 			
 			if (this.selected) {
 				computedClass += ' selected-text';
